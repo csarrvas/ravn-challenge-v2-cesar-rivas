bb012afcd4d6cc8fd28f88b180f439b1b9b25ea8
Cesar Rivas <crivas@applaudostudios.com>
2021-08-21 14:18:07 -0600

    🤡 Mock API response to style the components

---
+++ src/data/index.ts
@@ -0,0 +1,131 @@
+export default {
+  pageInfo: {
+    hasNextPage: true,
+    hasPreviousPage: false,
+  },
+  totalCount: 82,
+  edges: [
+    {
+      cursor: 'YXJyYXljb25uZWN0aW9uOjA=',
+    },
+    {
+      cursor: 'YXJyYXljb25uZWN0aW9uOjE=',
+    },
+    {
+      cursor: 'YXJyYXljb25uZWN0aW9uOjI=',
+    },
+    {
+      cursor: 'YXJyYXljb25uZWN0aW9uOjM=',
+    },
+    {
+      cursor: 'YXJyYXljb25uZWN0aW9uOjQ=',
+    },
+  ],
+  people: [
+    {
+      id: 'cGVvcGxlOjE=',
+      name: 'Luke Skywalker',
+      eyeColor: 'blue',
+      hairColor: 'blond',
+      skinColor: 'fair',
+      birthYear: '19BBY',
+      gender: 'male',
+      height: 172,
+      mass: 77,
+      species: null,
+      homeworld: {
+        name: 'Tatooine',
+      },
+      vehicleConnection: {
+        vehicles: [
+          {
+            name: 'Snowspeeder',
+          },
+          {
+            name: 'Imperial Speeder Bike',
+          },
+        ],
+      },
+    },
+    {
+      id: 'cGVvcGxlOjI=',
+      name: 'C-3PO',
+      eyeColor: 'yellow',
+      hairColor: 'n/a',
+      skinColor: 'gold',
+      birthYear: '112BBY',
+      gender: 'n/a',
+      height: 167,
+      mass: 75,
+      species: {
+        name: 'Droid',
+      },
+      homeworld: {
+        name: 'Tatooine',
+      },
+      vehicleConnection: {
+        vehicles: [],
+      },
+    },
+    {
+      id: 'cGVvcGxlOjM=',
+      name: 'R2-D2',
+      eyeColor: 'red',
+      hairColor: 'n/a',
+      skinColor: 'white, blue',
+      birthYear: '33BBY',
+      gender: 'n/a',
+      height: 96,
+      mass: 32,
+      species: {
+        name: 'Droid',
+      },
+      homeworld: {
+        name: 'Naboo',
+      },
+      vehicleConnection: {
+        vehicles: [],
+      },
+    },
+    {
+      id: 'cGVvcGxlOjQ=',
+      name: 'Darth Vader',
+      eyeColor: 'yellow',
+      hairColor: 'none',
+      skinColor: 'white',
+      birthYear: '41.9BBY',
+      gender: 'male',
+      height: 202,
+      mass: 136,
+      species: null,
+      homeworld: {
+        name: 'Tatooine',
+      },
+      vehicleConnection: {
+        vehicles: [],
+      },
+    },
+    {
+      id: 'cGVvcGxlOjU=',
+      name: 'Leia Organa',
+      eyeColor: 'brown',
+      hairColor: 'brown',
+      skinColor: 'light',
+      birthYear: '19BBY',
+      gender: 'female',
+      height: 150,
+      mass: 49,
+      species: null,
+      homeworld: {
+        name: 'Alderaan',
+      },
+      vehicleConnection: {
+        vehicles: [
+          {
+            name: 'Imperial Speeder Bike',
+          },
+        ],
+      },
+    },
+  ],
+};
