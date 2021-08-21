7ec6716c3fe8c367fc48c7560f306f4c0b48333b
Cesar Rivas <crivas@applaudostudios.com>
2021-08-21 11:58:45 -0600

    🚨 Fix eslint problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const App = (): React.ReactElement => (
-  <div>Ravn Challenge V2</div>
-);
+const App = (): React.ReactElement => <div>Ravn Challenge V2</div>;
 
 export default App;
